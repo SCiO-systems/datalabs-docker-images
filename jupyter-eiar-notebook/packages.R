db703b712b6a3cd66fdde69d06300fe3da8cfe59
Your Name <you@example.com>
2024-02-19 09:25:23 +0000

    adding R packages

---
+++ jupyter-eiar-notebook/packages.R
@@ -0,0 +1,7 @@
+install.packages(c('tidyverse', 'dplyr', 'stringr', 'ggplot2', 'ggpmisc', 'Metrics', 'lme4','geodata', 'lubridate',
+                    'MuMIn', 'rgdal', 'gridExtra', 'ggspatial', 'sf', 'plyr', 'sp','raster', 'foreach', 'parallel',
+                    'h2o', 'doParallel', 'Rquefts', 'limSolve', 'lpSolve', 'MuMIn', 'terra', 'randomForest', 'gridExtra', 
+                    'ggspatial', 'tmaptools', 'tmap', 'DSSAT', 'readxl', 'caret', 'tidymodels', 'tidyterra', 'leaflet', 'klippy'),
+    dependencies=TRUE, 
+    repos='http://cran.rstudio.com/', 
+    Ncpus = 4)
