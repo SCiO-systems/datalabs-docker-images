3e4d827bcdf2757511e4d6981d7157efdd5579fc
Your Name <you@example.com>
2024-02-29 11:04:00 +0000

    clearing R packages list

--- jupyter-eiaspatial-notebook/jammy/packages.R
+++ jupyter-eiaspatial-notebook/jammy/packages.R
@@ -1,15 +1,14 @@
-install.packages(c('devtools','raster', 'rgdal', 'rgeos', 'maptools', 'shapefiles', 
-                    'ncdf4', 'terra', 'rworldmap', 'spatstat.geom', 'ggmap', 'rgee', 
-                    'plotKML', 'leaflet', 'dismo', 'geodata', 'gdalUtilities', 'Hmisc', 
-                    'circlize', 'diagis', 'ggnewscale', 'lmerTest', 'lme4', 'metafor', 
-                    'metan', 'pbapply', 'FactoMineR', 'doParallel', 'ggpubr', 'broom', 
-                    'mlr3', 'RInside', 'doSNOW', 'rdrop2', 'Microsoft365R', 'googleCloudStorageR', 
+install.packages(c('devtools','raster', 'maptools', 'shapefiles', 'ncdf4', 'terra',
+                    'spatstat.geom', 'ggmap', 'rgee', 'leaflet', 'dismo', 'geodata',
+                    'circlize', 'diagis', 'ggnewscale', 'lmerTest', 'lme4', 'metafor',
+                    'metan', 'pbapply', 'FactoMineR', 'doParallel', 'ggpubr', 'broom',
+                    'mlr3', 'RInside', 'doSNOW', 'Microsoft365R', 'googleCloudStorageR',
                     'circular', 'miceadds', 'multimode', 'sfsmisc', 'rJava', 'R.utils', 
-                    'reshape2', 'rworldxtra', 'RCurl', 'tidyverse', 'ranger', 'sodium', 
-                    'pals', 'ggcorrplot', 'aws.s3', 'agricolae', 'xlsx', 'ecmwfr', 
-                    'envirem', 'ranger', 'maps', 'dplyr', 'DSSAT'), 
-    dependencies=TRUE, 
-    repos='http://cran.rstudio.com/', 
+                    'reshape2', 'rworldxtra', 'RCurl', 'tidyverse', 'ranger', 'sodium',
+                    'pals', 'ggcorrplot', 'aws.s3', 'agricolae', 'xlsx', 'ecmwfr', 'Hmisc',
+                    'envirem', 'maps', 'dplyr', 'DSSAT', 'rworldmap', 'gdalUtilities'),
+    dependencies=TRUE,
+    repos='http://cran.rstudio.com/',
     Ncpus = 4)
 
 devtools::install_github(c("CIAT-DAPA/analogues",
@@ -19,8 +18,7 @@ devtools::install_github(c("CIAT-DAPA/analogues",
                    "SantanderMetGroup/transformeR",
                    "SantanderMetGroup/visualizeR",
                    "SantanderMetGroup/downscaleR",
+                   "SantanderMetGroup/convertR",
+                   "SantanderMetGroup/climate4R.indices",
+                   "bakaburg1/BaySREn",
                    "femiguez/apsimx"), configure.args="Ncpus = 4")
-devtools::install_github(c("SantanderMetGroup/convertR",
-                   "SantanderMetGroup/climate4R.indices"))
-
-devtools::install_github("bakaburg1/BaySREn")
