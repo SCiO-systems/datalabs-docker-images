884949fe8e64fcf545b8550a6183e4512d2fe485
Your Name <you@example.com>
2024-03-11 14:56:53 +0000

    adding palderman/DSSAT

--- jupyter-eiar-notebook/packages.R
+++ jupyter-eiar-notebook/packages.R
@@ -1,9 +1,10 @@
 install.packages(c('devtools', 'remotes', 'tidyverse', 'dplyr', 'stringr', 'ggplot2', 'ggpmisc', 'Metrics', 'lme4','geodata', 'lubridate',
                     'MuMIn', 'gridExtra', 'ggspatial', 'sf', 'plyr', 'foreach', 'parallel', 'doParallel',
                     'h2o', 'Rquefts', 'limSolve', 'lpSolve', 'terra', 'randomForest', 'gridExtra', 'tmaptools',
-                    'tmap', 'DSSAT', 'readxl', 'caret', 'tidymodels', 'tidyterra', 'leaflet'),
+                    'tmap', 'readxl', 'caret', 'tidymodels', 'tidyterra', 'leaflet'),
     dependencies=TRUE, 
     repos='http://cran.rstudio.com/', 
     Ncpus = 4)
 
+devtools::install_github("palderman/DSSAT", ref = "develop")
 remotes::install_github("rlesur/klippy")
