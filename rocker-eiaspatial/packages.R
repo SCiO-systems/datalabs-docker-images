bf65ad392a93e5a43d50784d80bfab3ea77834c0
Odysseas <odysseas640@gmail.com>
2024-05-28 13:04:04 +0300

    0

--- rocker-eiaspatial/packages.R
+++ rocker-eiaspatial/packages.R
@@ -1,13 +1,13 @@
 install.packages(c('devtools', 'shapefiles', 
                     'ncdf4', 'terra', 'rworldmap', 'spatstat.geom', 'ggmap', 'rgee', 
-                    'leaflet', 'dismo', 'geodata', 'gdalUtilities', 'Hmisc', 
+                    'leaflet', 'dismo', 'gdalUtilities', 'Hmisc', 
                     'circlize', 'diagis', 'ggnewscale', 'lmerTest', 'lme4', 'metafor', 
                     'metan', 'pbapply', 'FactoMineR', 'doParallel', 'ggpubr', 'broom', 
                     'mlr3', 'RInside', 'doSNOW', 'Microsoft365R', 'googleCloudStorageR', 
                     'circular', 'miceadds', 'multimode', 'sfsmisc', 'rJava', 'R.utils', 
                     'reshape2', 'rworldxtra', 'RCurl', 'tidyverse', 'ranger', 'sodium', 
                     'pals', 'ggcorrplot', 'aws.s3', 'agricolae', 'xlsx', 'ecmwfr', 
-                    'envirem', 'ranger', 'maps', 'dplyr', 'DSSAT'), 
+                    'envirem', 'ranger', 'maps', 'dplyr', 'DSSAT', Ncpus = 4), 
     dependencies=TRUE, 
     repos='http://cran.rstudio.com/')
 
@@ -20,6 +20,6 @@ devtools::install_github(c("CIAT-DAPA/analogues",
                    "SantanderMetGroup/downscaleR",
                    "femiguez/apsimx"))
 devtools::install_github(c("SantanderMetGroup/convertR",
-                   "SantanderMetGroup/climate4R.indices"))
+                   "SantanderMetGroup/climate4R.indices"), Ncpus = 4)
 
 devtools::install_github("bakaburg1/BaySREn")
